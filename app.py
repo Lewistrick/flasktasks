67fd5a17383ed9cd726e6b984d5b9c36a4121cc8
e.wilts <e.wilts@ncim.nl>
2023-12-08 14:36:06 +0100

    Add route for getting all tasks

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from flasktasks.resources import task_resources
 app = Flask(__name__)
 api = Api(app)
 
+# Add resources for the API. Resources are defined by a resource class and an endpoint.
+# A resource class, implements all methods that are allowed, e.g. get().
+#
+api.add_resource(task_resources.TasksGetter, "/tasks")
 api.add_resource(task_resources.TaskGetter, "/task/<int:id>")
 
 if __name__ == "__main__":
