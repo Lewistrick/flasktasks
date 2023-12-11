38ddaa3dea6325f0f8976849a4444c85605a0e0c
Lewistrick <erickwilts@gmail.com>
2023-12-11 15:15:07 +0100

    Docstrings for test functions

--- flasktasks/tests/test_database.py
+++ flasktasks/tests/test_database.py
@@ -18,6 +18,7 @@ from flasktasks.tests.mocks import (
 
 
 def test_get_task(monkeypatch: MonkeyPatch, mocker: MockerFixture):
+    """Test if getting one task is possible"""
     mocker.patch("flasktasks.database.task.select")
     mocker.patch("flasktasks.database.task.engine")
     monkeypatch.setattr("flasktasks.database.task.Session", MockSession)
@@ -28,6 +29,7 @@ def test_get_task(monkeypatch: MonkeyPatch, mocker: MockerFixture):
     assert task.due_date == "2000-01-01"
 
 def test_get_nonexistent_task(monkeypatch: MonkeyPatch, mocker: MockerFixture):
+    """Test if getting a task with ID that doesn't exist returns None"""
     mocker.patch("flasktasks.database.task.select")
     mocker.patch("flasktasks.database.task.engine")
     monkeypatch.setattr("flasktasks.database.task.Session", MockSession)
@@ -36,6 +38,7 @@ def test_get_nonexistent_task(monkeypatch: MonkeyPatch, mocker: MockerFixture):
     assert task is None
 
 def test_get_all_tasks(monkeypatch: MonkeyPatch, mocker: MockerFixture):
+    """Test if getting all tasks is possible"""
     mocker.patch("flasktasks.database.task.select")
     mocker.patch("flasktasks.database.task.engine")
     monkeypatch.setattr("flasktasks.database.task.Session", MockSession)
@@ -44,11 +47,13 @@ def test_get_all_tasks(monkeypatch: MonkeyPatch, mocker: MockerFixture):
     assert all(task.due_date == f"2000-01-{task.task_id:02d}" for task in tasks)
 
 def test_update_task(monkeypatch: MonkeyPatch, mock_task):
+    """Test if updating one task is possible"""
     monkeypatch.setattr("flasktasks.database.task.Session", MockSession)
     new_task = update_by_id(mock_task.task_id, title="new value")
     assert new_task.title == "new value"
 
 def test_update_task_invalid_field(monkeypatch: MonkeyPatch, mocker: MockerFixture):
+    """Test if updating a task field that doesn't exist raises an exception"""
     monkeypatch.setattr("flasktasks.database.task.Session", MockSession)
     task = TaskRecord(task_id=123, title="", due_date="2000-01-01")
     mocker.patch("flasktasks.database.task.select_by_id", lambda task_id: task)
@@ -56,12 +61,18 @@ def test_update_task_invalid_field(monkeypatch: MonkeyPatch, mocker: MockerFixtu
         update_by_id(task.task_id, invalid_field="new value")
 
 def test_search(monkeypatch: MonkeyPatch):
+    """Test if searching works"""
     monkeypatch.setattr("flasktasks.database.task.Session", MockSession)
     monkeypatch.setattr("flasktasks.database.task.select", get_mock_query_result)
     for expected, actual in zip(get_mock_query_result(), search_by_query("test")):
         assert expected == actual
 
 def test_search_sort(monkeypatch: MonkeyPatch, mock_task):
+    """Showcase that sorting in search is supported; this does not actually sort.
+    
+    The SQLModel library has extensive tests internally:
+    https://github.com/tiangolo/sqlmodel/tree/main/tests
+    """
     monkeypatch.setattr("flasktasks.database.task.Session", MockSession)
     monkeypatch.setattr("flasktasks.database.task.select", MockQueryResultSet)
     monkeypatch.setattr("flasktasks.database.task.TaskRecord", mock_task)
